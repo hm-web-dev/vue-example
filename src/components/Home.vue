fcedd4d3d1b441ea9fc8589b687e3fea1e76f0b0
Carmen Zhang <jwzhang06@gmail.com>
2023-03-09 09:35:11 -0500

    toppings

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -15,6 +15,22 @@ export default {
                 {
                     src: "https://images.squarespace-cdn.com/content/v1/5dd8630d09ab5908e35b35a0/1582497960940-IAE86F3OGQKE0D19GGYA/ketchup+dog.jpg?format=1000w",
                     hotdog: "ketchup"
+                },
+                {
+                    src: "https://images.squarespace-cdn.com/content/v1/5dd8630d09ab5908e35b35a0/1586969319681-4OLWCUAYUSSWHGES6IOY/sprinkle+dog.jpg?format=1000w",
+                    hotdog: "sprinkle"
+                },
+                {
+                    src: "https://images.squarespace-cdn.com/content/v1/5dd8630d09ab5908e35b35a0/1603992035188-Y4LE9KCNN2IDIKG6OASN/hat+dog.png?format=1000w",
+                    hotdog: "hat"
+                },
+                {
+                    src: "https://images.squarespace-cdn.com/content/v1/5dd8630d09ab5908e35b35a0/1590525011183-J6RNSXLQT2BZV22F5I6V/ball+dog.jpg?format=1000w",
+                    hotdog: "basketball"
+                },
+                {
+                    src: "https://images.squarespace-cdn.com/content/v1/5dd8630d09ab5908e35b35a0/1593362109648-FYSE9T1HR5KEPVE7O9MN/nacho+dog.jpg?format=1000w",
+                    hotdog: "nachos"
                 }
             ];
             const res = otherDogs.find(h => h.hotdog == this.hotdog);
@@ -52,6 +68,20 @@ export default {
             <a v-on:click="e => changeData(otherDogs[2])">
                 Put some {{ otherDogs[2].hotdog }} on it
             </a>
+
+            <a v-on:click="e => changeData(otherDogs[3])">
+                Put some {{ otherDogs[3].hotdog }} on it
+            </a>
+            <a v-on:click="e => changeData(otherDogs[4])">
+                Put some {{ otherDogs[4].hotdog }} on it
+            </a>
+            <a v-on:click="e => changeData(otherDogs[5])">
+                Put some {{ otherDogs[5].hotdog }} on it
+            </a>
+            <a v-on:click="e => changeData(otherDogs[6])">
+                Put some {{ otherDogs[6].hotdog }} on it
+            </a>
+
         </div>
     </body>
 </template>
